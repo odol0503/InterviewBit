2172e1b897ed95f42748b134150b57afc8dfc963
sukhoon.oh <sukhoon.oh@nexstreaming.com>
2018-01-04 18:13:29 +0900

    FindDuplicate.cpp

--- ib/FindDuplicate.cpp
+++ ib/FindDuplicate.cpp
@@ -10,22 +10,9 @@ Sample Output:
 1
 If there are multiple possible answers ( like in the sample case above ), output any one.
 If there is no duplicate, output -1
-
-Split the numbers from 1 to n in sqrt(n) ranges so that range i corresponds to [sqrt(n) * i .. sqrt(n) * (i + 1)).
-
-Do one pass through the stream of numbers and figure out how many numbers fall in each of the ranges.
-
-At least one of the ranges will contain more than sqrt(n) elements.
-
-Do another pass and process just those elements in the oversubscribed range.
-
-Using a hash table to keep frequencies, you¡¯ll find a repeated element.
-
-This is O(sqrt(n)) memory and 2 sequential passes through the stream.
 *************************************************************************************************/
 #include <vector>
 #include <cmath>
-#include <memory>
 using namespace std;
 
 int FindDuplicate(vector<int> &V)
@@ -34,8 +21,7 @@ int FindDuplicate(vector<int> &V)
 	int valueRange = V.size() - 1; // 1 to N when the size is N+1.
 	int range = sqrt(valueRange);
 	if (range * range < valueRange) range++;
-	shared_ptr<int> count(new int(range + 1));
-	memset((void*)count, 0, sizeof(count));
+	vector<int> count(range + 1, 0);
 
 	for (int i = 0; i < V.size(); i++) {
 		count[(V[i] - 1) / range]++;
@@ -52,7 +38,10 @@ int FindDuplicate(vector<int> &V)
 		}
 	}
 	if (repeatingRange == -1) return -1;
-	memset(count, 0, sizeof(count));
+	for (int i = 0; i < count.size(); i++)
+	{
+		count[i] = 0;
+	}
 	for (int i = 0; i < V.size(); i++) {
 		if ((V[i] - 1) / range == repeatingRange) count[(V[i] - 1) % range]++;
 	}
