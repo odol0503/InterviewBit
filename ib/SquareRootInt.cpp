80ff271fa1b22b2e8fee5709b23722f66cbc9d36
sukhoon.oh <sukhoon.oh@nexstreaming.com>
2018-01-19 17:36:30 +0900

    Add SqaurRootInt

---
+++ ib/SquareRootInt.cpp
@@ -0,0 +1,61 @@
+/*************************************************************************************************
+Square Root of Integer
+
+Implement int sqrt(int x).
+Compute and return the square root of x.
+If x is not a perfect square, return floor(sqrt(x))
+
+Example :
+Input : 11
+Output : 3
+DO NOT USE SQRT FUNCTION FROM STANDARD LIBRARY
+*************************************************************************************************/
+#include <cmath>
+
+#define OWN
+
+#ifdef OWN
+int SqrtInt(int A) 
+{
+	if (A < 2) return A;
+	long long i = A / 2;
+	long long start = 0;
+	long long end = A;
+	while (i*i > A || A >= (i+1)*(i+1))
+	{
+		if (i == 1) break;
+		
+		if (i*i < A)
+		{
+			long long diff = end - i;
+			start = i;
+			i += (long long)(diff / 2);
+		}
+		else if (i*i > A)
+		{
+			long long diff = i - start;
+			end = i;
+			i -= (long long)(diff / 2);
+		}
+	}
+
+	return (int)i;
+}
+#else
+int SqrtInt(int x)
+{
+	if (x == 0) return 0;
+	int start = 1, end = x, ans;
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		if (mid <= x / mid) {
+			start = mid + 1;
+			ans = mid;
+		}
+		else {
+			end = mid - 1;
+		}
+	}
+	return ans;
+}
+#endif
