6869c663a3d8199dac9e7a96e023ecb4456e8fd9
sukhoon.oh <sukhoon.oh@nexstreaming.com>
2018-03-06 11:23:50 +0900

    Add WindowString.cpp

--- ib/level5/Hashing/WindowString.cpp
+++ ib/level5/Hashing/WindowString.cpp
@@ -20,6 +20,8 @@ If there are multiple such windows, return the first occurring minimum window
 #include <map>
 #include <algorithm>
 #include <vector>
+#include <set>
+#include <unordered_map>
 using namespace std;
 
 #define OWN
@@ -30,27 +32,101 @@ string MinWindow(string A, string B) {
 	int num = (int)B.size();
 	int min_len = INT_MAX;
 	string ret;
-	int Bmap[256];
-	int Smap[256];
+	int BMap[256];
+	set<int> SMap;
 
-	for (int i = 0; i < 256; i++)
+	for (int i = 0; i<256; i++)
 	{
-		Bmap[i] = 0;
-		Smap[i] = 0;
+		BMap[i] = 0;
 	}
 
-	for (int i = 0; i < num; i++)
+	for (int i = 0; i<num; i++)
 	{
-		Bmap[B[i]]++;
+		BMap[B[i]]++;
+		SMap.insert(B[i]);
 	}
-	
-	for (int i = 0; i < len; i++)
-	{
 
+	int start = 0;
+	int end = 0;
+
+	while (start <= end && end < len)
+	{
+		if (SMap.count(A[end]))
+		{
+			if (num > 0)
+			{
+				if (BMap[A[end]] > 0) num--;
+				BMap[A[end]]--;
+
+				while (num == 0)
+				{
+					int gap = end - start + 1;
+					if (min_len > gap)
+					{
+						min_len = gap;
+						ret = A.substr(start, min_len);
+					}
+
+					if (SMap.count(A[start]))
+					{
+						if (BMap[A[start]] >= 0) num++;
+						BMap[A[start]]++;
+					}
+					while (SMap.count(A[++start]) == 0);
+				}
+			}
+		}
+		end++;
 	}
 
 	return ret;
 }
 #else
-
+string MinWindow(string S, string T) {
+	if (S.length() < T.length()) return "";
+	unordered_map<char, int> counts;
+	for (int i = 0; i < T.length(); i++) {
+		if (counts.find(T[i]) == counts.end()) {
+			counts[T[i]] = 1;
+		}
+		else {
+			counts[T[i]]++;
+		}
+	}
+	int start = 0;
+	int length = 0;
+	int total = 0;
+	for (int head = 0, tail = 0; tail < S.length(); tail++) {
+		auto itTail = counts.find(S[tail]);
+		if (counts.find(S[tail]) == counts.end()) {
+			// If this character is not present in T at all, 
+			// we don't care about this character. 
+			continue;
+	}
+		counts[S[tail]]--;
+		// We check if the current character represented by tail caused
+		// a character to be included which is relevant to T and is still
+		// in deficit. 
+		// For example, if T has 3 As, then the first 3 A are relevant to us
+		// but the 4th one is not. 
+		if (counts[S[tail]] >= 0) {
+			total++;
+		}
+		// check if we have all characters in T covered. 
+		if (total == T.size()) {
+			// Now move the head pointer till popping out those characters 
+			// still makes sure that all characters in T are covered. 
+			while (counts.find(S[head]) == counts.end() || counts[S[head]] < 0) {
+				if (counts.find(S[head]) != counts.end()) counts[S[head]]++;
+				head++;
+			}
+			// Now [head - 1, tail] is a valid substring. Update the answer. 
+			if (length == 0 || tail - head + 1 < length) {
+				length = tail - head + 1;
+				start = head;
+			}
+		}
+	}
+	return S.substr(start, length);
+}
 #endif
