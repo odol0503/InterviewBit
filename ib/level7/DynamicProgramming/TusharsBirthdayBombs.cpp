5d15e47b613f741c6bea4b2ee6b0deda79f03b0f
sukhoon.oh <sukhoon.oh@nexstreaming.com>
2018-04-12 15:10:28 +0900

    Modify TusharsBirthdayBombs.cpp

--- ib/level7/DynamicProgramming/TusharsBirthdayBombs.cpp
+++ ib/level7/DynamicProgramming/TusharsBirthdayBombs.cpp
@@ -1,30 +1,27 @@
 /*************************************************************************************************
-Largest area of rectangle with permutations
+Tushar's Birthday Bombs
 
-Given a binary grid i.e. a 2D grid only consisting of 0¡¯s and 1¡¯s, find the area of
-the largest rectangle inside the grid such that all the cells inside the chosen rectangle
-should have 1 in them. You are allowed to permutate the columns matrix i.e.
-you can arrange each of the column in any order in the final grid.
-Please follow the below example for more clarity.
+It¡¯s Tushar¡¯s birthday today and he has N friends. Friends are numbered [0, 1, 2, ¡¦., N-1] 
+and i-th friend have a positive strength S(i). Today being his birthday, his friends have 
+planned to give him birthday bombs (kicks :P). Tushar¡¯s friends know Tushar¡¯s pain bearing 
+limit and would hit accordingly.
+If Tushar¡¯s resistance is denoted by R (>=0) then find the lexicographically smallest order of 
+friends to kick Tushar so that the cumulative kick strength (sum of the strengths of friends
+who kicks) doesn¡¯t exceed his resistance capacity and total no. of kicks hit are maximum. 
+Also note that each friend can kick unlimited number of times (If a friend hits x times, 
+his strength will be counted x times)
 
-Lets say we are given a binary grid of 3 * 3 size.
-1 0 1
-0 1 0
-1 0 0
-
-At present we can see that max rectangle satisfying the criteria mentioned in the problem
-is of 1 * 1 = 1 area i.e either of the 4 cells which contain 1 in it.
-Now since we are allowed to permutate the columns of the given matrix,
-we can take column 1 and column 3 and make them neighbours.
-One of the possible configuration of the grid can be:
-1 1 0
-0 0 1
-1 0 0
-
-Now In this grid, first column is column 1, second column is column 3 and third column
-is column 2 from the original given grid. Now, we can see that if we calculate the max
-area rectangle, we get max area as 1 * 2 = 2 which is bigger than the earlier case.
-Hence 2 will be the answer in this case.
+Note:
+Answer format = Vector of indexes of friends in the order in which they will hit.
+Answer should have the maximum no. of kicks that can be possibly hit. If two answer have 
+the same no. of kicks, return one with the lexicographically smaller.
+[a1, a2, ¡¦., am] is lexicographically smaller than [b1, b2, .., bm] 
+if a1 < b1 or (a1 = b1 and a2 < b2) ¡¦ .
+Input cases are such that the length of the answer does not exceed 100000.
+Example:
+R = 11, S = [6,8,5,4,7]
+ans = [0,2]
+Here, [2,3], [2,2] or [3,3] also give the maximum no. kicks.
 *************************************************************************************************/
 #include <vector>
 #include <utility>
@@ -35,17 +32,17 @@ using namespace std;
 #define OWN
 
 #ifdef OWN
-void kick(int A, vector<int> &B, int pos, vector<int> &list, vector<int> &ret)
+void kick(int A, vector<pair<int, int>> &C, int pos, vector<int> &list, vector<int> &ret)
 {
-	for (int i = pos; i<(int)B.size(); i++)
+	for (int i = pos; i<(int)C.size(); i++)
 	{
-		if (A - B[i] < 0)
+		if (A - C[i].second < 0)
 		{
 			if (list.size() > ret.size()) ret.assign(list.begin(), list.end());
 			continue;
 		}
-		list.push_back(i);
-		kick(A - B[i], B, i, list, ret);
+		list.push_back(C[i].first);
+		kick(A - C[i].second, C, i, list, ret);
 		list.pop_back();
 	}
 }
@@ -53,11 +50,74 @@ void kick(int A, vector<int> &B, int pos, vector<int> &list, vector<int> &ret)
 vector<int> TusharsBirthdayBombs(int A, vector<int> &B) {
 	vector<int> list;
 	vector<int> ret;
-	kick(A, B, 0, list, ret);
+	vector<pair<int, int>> C;
+
+	for (int i = 0; i<(int)B.size(); i++)
+	{
+		if (B[i] <= A)
+		{
+			C.push_back({ i, B[i] });
+		}
+	}
+
+	kick(A, C, 0, list, ret);
 	int max_len = 0;
 	int max_idx = -1;
 	return ret;
 }
-#else
+#elif defined(OWN2)
+vector<int> TusharsBirthdayBombs(int A, vector<int> &B) {
+	vector<int> ret;
+	int min_val = *min_element(B.begin(), B.end());
 
+	for (int i = 0; i<B.size(); i++)
+	{
+		if (B[i] <= A && (A - B[i]) / min_val + 1 == A / min_val)
+		{
+			ret.push_back(i);
+			A -= B[i];
+			i--;
+		}
+	}
+
+	return ret;
+}
+#else
+#define F first
+#define S second
+
+vector<int> TusharsBirthdayBombs(int A, vector<int>& B) {
+	// newB -> only those strengths which are useful
+	// pair {i,j} -> i = original index of the person, j = strength
+	vector<pair<int, int> > newB;
+
+	int prev = INT_MAX;
+	for (int i = 0; i<B.size(); i++) {
+		if (B[i] < prev) {
+			newB.push_back({ i, B[i] });
+			prev = B[i];
+		}
+	}
+
+	int n = newB.size();
+
+	vector<int> ans;
+
+	int R = A;
+	int temp = newB[n - 1].second;   //smallest strength
+	int i = 0;
+
+
+	while (i<n) {
+		// check if the current person can hit and if he can hit
+		// would he change the answer or not
+		if (R >= newB[i].S && (1 + (R - newB[i].S) / temp) == R / temp) {
+			ans.push_back(newB[i].F);
+			R -= newB[i].S;
+		}
+		else i++;
+	}
+
+	return ans;
+}
 #endif
